1210281962d2704f7dde9fc085ca83aad8aab37c
Filter <not.so.filter@hotmail.com>
2025-12-18 15:16:12 -0500

    fix small error with make script

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 @echo off
-if exist smb.nes move /y smb.unh smb.prev.unh >NUL
+if exist smb.unh move /y smb.unh smb.prev.unh >NUL
 tool\windows\vasm6502_psi-x -maxerrors=0 -noalign -Fbin -start=0 -o smb.unh -L smb.lst -Lall smb.asm 2> errors.log
 pause
